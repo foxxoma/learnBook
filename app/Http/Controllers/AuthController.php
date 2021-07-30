393035e5267d114764113bb7f433fc026d9201a9
andrey <foxxoma@gmail.com>
2021-07-30 14:37:08 +0300

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,5 +10,13 @@ use App\User;
 
 class AuthController extends Controller
 {
-    //
+	protected function create(array $data)
+	{
+		return User::create([
+			'name' => $data['name'],
+			'email' => $data['email'],
+			'password' => Hash::make($data['password']),
+			'api_token' => Str::random(60),
+		]);
+	}
 }
--- config/auth.php
+++ config/auth.php
@@ -2,6 +2,11 @@
 
 return [
 
+    'api' => [
+        'driver' => 'token',
+        'provider' => 'users',
+        'hash' => true,
+    ],
     /*
     |--------------------------------------------------------------------------
     | Authentication Defaults
